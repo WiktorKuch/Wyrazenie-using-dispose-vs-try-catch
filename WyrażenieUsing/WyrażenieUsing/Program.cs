804bfbcb168539e8fd55e23a3049b9476f7ca13f
WiktorKuch <wiktor.kucharek1997@gmail.com>
2022-12-04 16:04:23 +0100

    wyrazenie using

---
+++ WyrażenieUsing/WyrażenieUsing/Program.cs
@@ -0,0 +1,47 @@
+﻿using WyrażenieUsing;
+
+namespace WyrazenieUsing{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filePath = "C:\\Users\\Wiktro Kucharek\append\fileUsing.txt";
+            var fileContent = File.ReadAllLines(filePath);
+
+            using(var someClass = new SomeClass())
+            {
+                someClass.Say("Hi");
+            }
+
+
+
+
+            using (var readFileStream = new FileStream(filePath, FileMode.Open)) // do zmiennej przypisujemy stworzony obiekt 
+            {                                                                     //a do jego konstruktora ścieżke oraz tryb otwarcia
+                                                                                  // using zastępuje blok try catch oraz throw new Exception()  
+              //readFileStream.Read();                                          // blok using mozemy wykorzystać dla każdej klasy która implementuje 
+                                                                                  // interface IDisposable- tam metoda void Dispose() - zwalnia zasoby
+
+            }
+
+
+
+
+            var writeFileStream = new FileStream(filePath, FileMode.Open);
+            try
+            {
+                // writeFileStream.Write();
+            }
+            finally
+            {
+                ((IDisposable)writeFileStream).Dispose();
+            }
+
+
+            writeFileStream.Close();
+
+        }
+    }
+
+}
